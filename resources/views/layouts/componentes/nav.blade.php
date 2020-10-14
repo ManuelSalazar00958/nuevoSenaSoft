822e3a95a24e5efb872a3a6a6d77ce272eafc0de
JuanCa7 <sandovaljuanca74@gmail.com>
2020-10-14 11:17:28 -0500

    Inicio del proyecto GestionFacturacion

---
+++ resources/views/layouts/componentes/nav.blade.php
@@ -0,0 +1,20 @@
+<div class="d-flex flex-column">
+    <!--redes sociales-->
+    <div class="profile">
+      <img src="assets/img/profile-img.jpg" alt="" class="img-fluid rounded-circle">
+      <h1 class="text-light"><a href="index.html">Tiendas amigo</a></h1>
+    </div>
+    <!--navegacion-->
+    <nav class="nav-menu">
+      <ul>
+        <li class="active"><a href="index.html"><i class="bx bx-home"></i> <span>hogar</span></a></li>
+        <li><a href="#about"><i class="bx bx-user"></i> <span>acerca de</span></a></li>
+        <li><a href="#resume"><i class="bx bx-file-blank"></i> <span>Resumen</span></a></li>
+        <li><a href="#portfolio"><i class="bx bx-book-content"></i> Productos</a></li>
+        <li><a href="#services"><i class="bx bx-server"></i> Servicos</a></li>
+        <li><a href="#contact"><i class="bx bx-envelope"></i> Contactos</a></li>
+
+      </ul>
+    </nav><!-- .nav-menu -->
+    <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+  </div>
